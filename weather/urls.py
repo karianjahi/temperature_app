b254d62160ce4b39349cd544667b79e3c772957e
joseph <61469887+karianjahi@users.noreply.github.com>
2022-03-21 11:02:03 +0100

    Make some changes after pasilla encounter

--- weather/urls.py
+++ weather/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.urls import re_path as url
 from temperature.views import WeatherList, WeatherDetail
 
 urlpatterns = [
